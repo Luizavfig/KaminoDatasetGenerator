03e2f1fe9325752d0206d5b0b6542bcd0d7e7ac5
lucianoMarchezan <lucianomarchezan94@gmail.com>
2026-01-19 12:59:09 -0500

    minor fix to avoid repeating eval

--- pipeline/src/rq3.py
+++ pipeline/src/rq3.py
@@ -32,5 +32,5 @@ if __name__ == "__main__":
     for th in threshold:    
         #run_clone_evaluation(str(model_output_dir), full_model_name, threshold=th, dataset_name="Kamino", language="python") # for our own dataset
         run_clone_evaluation(str(model_output_dir), full_model_name, threshold=th, dataset_name="BigCloneBench", language="java") # for BigCloneBench
-        for language in languages: # for GPTCloneBench
-            run_clone_evaluation(str(model_output_dir), full_model_name, dataset_name="GPTCloneBench",language=language, threshold=th)
+        #for language in languages: # for GPTCloneBench
+         #   run_clone_evaluation(str(model_output_dir), full_model_name, dataset_name="GPTCloneBench",language=language, threshold=th)
